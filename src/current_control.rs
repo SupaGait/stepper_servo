eef0a9976d4e3e1214ffda83293149e66a7d9b06
SupaGait <gklomphaar@hotmail.com>
2020-04-21 19:48:45 +0200

    Updated with latest hal trunk - Pwm modification necessary - formatting.

--- src/current_control.rs
+++ src/current_control.rs
@@ -1,8 +1,11 @@
 use embedded_hal::PwmPin;
+
+
 use embedded_hal::digital::v2::OutputPin;
 use motor_driver as motor;
-use motor::ic::L298;
+use l298n;
 use crate::pid::{PIDController, Controller, util};
+
 
 #[derive(PartialEq)]
 enum Direction { CCW, CW, }
@@ -22,7 +25,7 @@ where
     voltage: u32,
     current: u32,
     duty_cycle: u32,
-    motor: motor::Motor<IN1, IN2, PWM, L298>,
+     motor: l298n::Motor<IN1, IN2, PWM>,
     direction: Direction,
     pid: PIDController<i32>,
 
@@ -37,7 +40,7 @@ where
     IN2: OutputPin,
 {
     pub fn new(
-        shunt_resistance: u32,
+        shunt_resistance: f32,
         pwm: PWM,
         in1: IN1,
         in2: IN2,
@@ -49,9 +52,10 @@ where
             voltage: 0,
             current: 0,
             duty_cycle: 0,
-            motor: motor::Motor::l298(in1, in2, pwm),
+            motor: l298n::Motor::new(in1, in2, pwm),
             direction: Direction::CW,
-            pid: PIDController::new(80, 0, 0),  // PID
+            //pid: PIDController::new(50.0, 1.0, 0.0),  // PID
+            pid: PIDController::new(100.0, 0.0, 0.0),  // PID
 
             current_buffer: [0; CURRENT_BUFFER_SIZE],
             buffer_index: 0,
@@ -60,7 +64,7 @@ where
         //s.set_duty_cycle(220);
         //s.pid.set_limits(0.0, s.motor.get_max_duty() as f32);
         s.pid.set_limits(0, 230);
-        s.motor.cw();
+        s.motor.forward();
         s
     }
 
@@ -86,15 +90,15 @@ where
         self.current
     }
 
-    pub fn update(&mut self, dt:u32, adc_value: u32, adc_voltage: u32)
+    pub fn update(&mut self, dt:f32, adc_value: u16, adc_voltage: f32)
     {
         self.adc_value = adc_value;
         self.voltage = adc_voltage;
 
         self.set_direction();
 
-        let mut current = 0;
-        if self.voltage > 0
+        let mut current = 0.0;
+        if self.adc_value > 0
         {
             current = (self.voltage * 1000) / self.shunt_resistance; // uV / mOhm = mA
         }
@@ -120,28 +124,30 @@ where
 
     fn set_direction(&mut self)
     {
-        if self.current_setpoint >= 0
+        if self.current_setpoint >= 0.0
         { 
             if self.direction  != Direction::CW {
-                self.motor.cw();
+                self.motor.forward();
                 self.direction = Direction::CW;
             }
         }
         else
         {
             if self.direction != Direction::CCW {
-                self.motor.ccw();
+                self.motor.reverse();
                 self.direction = Direction::CCW;
             }
         }
     }
 
-    fn set_duty_cycle(&mut self, duty_cycle : u32) {
-        self.duty_cycle = duty_cycle;
-        self.motor.duty(self.duty_cycle as u16);
+    fn set_duty_cycle(&mut self, duty_cycle : f32) {
+        self.duty_cycle_raw = duty_cycle;
+        self.duty_cycle = (duty_cycle as u16);//.integer_sqrt();
+
+        self.motor.set_duty(self.duty_cycle);
     }
 
-    fn calc_pwm(&mut self, dt: u32) 
+    fn calc_pwm(&mut self, dt: f32) 
     {
         let mut duty_cycle = self.duty_cycle as i32;
         duty_cycle += self.pid.update(self.current as i32, dt as i32) / PID_SCALING_FACTOR;
